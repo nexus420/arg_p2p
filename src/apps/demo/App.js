b68f8a380dba64d7cda534f4e568edc2f3170e6f
Brett Sun <qisheng.brett.sun@gmail.com>
2018-02-04 00:31:53 +0100

    Demo: add background worker and messaging

--- src/apps/demo/App.js
+++ src/apps/demo/App.js
@@ -1,24 +1,88 @@
 import React from 'react'
 import styled from 'styled-components'
-import { AragonApp, EmptyStateCard, IconSettings } from '@aragon/ui'
+import { AragonApp, Button, EmptyStateCard, IconSettings } from '@aragon/ui'
 
-const App = () => (
-  <AragonApp publicUrl="/aragon-ui/">
-    <Main>
-      <EmptyStateCard
-        actionText="This is a demo!"
-        icon={IconSettings}
-        title="Demo app"
-      />
-    </Main>
-  </AragonApp>
-)
+class App extends React.Component {
+  state = {
+    counter: 0,
+  }
+  componentWillMount() {
+    window.addEventListener('message', this.handleReceiveMessage, false)
+    this.sendMessage({
+      method: 'cache',
+      payload: {
+        mode: 'get',
+        key: 'counter',
+      },
+    })
+  }
+  componentWillUnmount() {
+    window.removeEventListener('message', this.handleReceiveMessage, false)
+  }
+  handleReceiveMessage = ({ data: { method, payload } }) => {
+    if (method === 'cache') {
+      this.handleReceiveState(payload)
+    }
+  }
+  handleReceiveState = ({ key, value }) => {
+    this.setState({ [key]: value })
+  }
+  handleSendCall = event => {
+    this.sendMessage({
+      method: 'call',
+      payload: {},
+    })
+  }
+  handleSendError = event => {
+    this.sendMessage({
+      method: 'error',
+      payload: {},
+    })
+  }
+  sendMessage = event => {
+    window.parent.postMessage(event, '*')
+  }
+  render() {
+    const { counter } = this.state
+    return (
+      <AragonApp publicUrl="/aragon-ui/">
+        <Main>
+          <EmptyStateCard
+            actionText={'This is a demo!'}
+            icon={IconSettings}
+            title={`Demo app state: ${counter}`}
+          />
+          <Actions>
+            <Button mode="strong" onClick={this.handleSendCall}>
+              Send Call
+            </Button>
+            <Button mode="secondary" onClick={this.handleSendError}>
+              Send Error
+            </Button>
+          </Actions>
+        </Main>
+      </AragonApp>
+    )
+  }
+}
 
 const Main = styled.div`
-  display: flex;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`
+
+const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+
+  & > *:first-child {
+    margin-right: 10px;
+  }
 `
 
 export default App
